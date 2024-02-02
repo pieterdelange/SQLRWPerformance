2edbe5d938dd24e909e8e4d811fa2f417b277b5c
Peter <pdelange@pdelange.com>
2024-02-02 16:33:38 +0100

    Added testfile creation and package

---
+++ CreateTestfile.ps1
@@ -0,0 +1,47 @@
+﻿#Delete existing testfiles directory and create a new one
+$FoldernameToCreate = 'Testfiles'
+$Root = 'C:'
+$FullPathToFolderToCreate = Join-Path -Path $Root -ChildPath $FoldernameToCreate
+Write-Host "Directory to create: $($FullPathToFolderToCreate)"
+
+if(Test-Path -path  $FullPathToFolderToCreate) {
+    Remove-Item $FullPathToFolderToCreate
+}
+
+New-Item -ItemType Directory -Path $FullPathToFolderToCreate 
+
+#Let's write some content to a file in our new directory. 
+$FileNameToWriteTo = "Test.csv"
+#Let's create the full path
+$PathToWriteTo = Join-Path -path $FullPathToFolderToCreate -ChildPath $FileNameToWriteTo
+Write-Host "Path to write to: $($PathToWriteTo)"
+
+#Determine nr of lines to write
+[bigint]$counter = 0
+[bigint]$NrOfLinesToWrite = 10000000
+
+#$sw = [io.file]::AppendText($PathToWriteTo);
+$sw =[System.IO.StreamWriter] $PathToWriteTo;
+
+$StopWatch = new-object system.diagnostics.stopwatch
+$StopWatch.Start()
+
+#Kudos to PowerShell administrator blog
+#https://powershelladministrator.com/2015/11/15/speed-of-loops-and-different-ways-of-writing-to-files-which-is-the-quickest/
+do
+{
+    $sw.WriteLine("$($counter);textcolumn1XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX${$counter};textcolumn2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX${$counter};textcolumn3XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX${$counter};textcolumn4XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX${$counter}")
+    if ($counter%($NrOfLinesToWrite / 100) -eq 0) {
+        Write-Host "Completed writing $($counter) rows"
+    }
+    $counter = $counter + 1
+}
+while ($counter -lt $NrOfLinesToWrite)
+
+$sw.Close()
+$sw.Dispose()
+
+$StopWatch.Stop()
+$Minutes = $StopWatch.Elapsed.TotalMinutes
+
+$StopWatch.Elapsed.Seconds
--- RWPerformanceDB/Tables/Store.sql
+++ RWPerformanceDB/Tables/Store.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Store]
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY, 
-    [TextColumn1] VARCHAR(500) NOT NULL,
-	[TextColumn2] VARCHAR(500) NOT NULL,
-	[TextColumn3] VARCHAR(500) NOT NULL,
-	[TextColumn4] VARCHAR(500) NOT NULL
+    [TextColumn1] NVARCHAR(500) NOT NULL,
+	[TextColumn2] NVARCHAR(500) NOT NULL,
+	[TextColumn3] NVARCHAR(500) NOT NULL,
+	[TextColumn4] NVARCHAR(500) NOT NULL
 ) ON FileGroupRW
